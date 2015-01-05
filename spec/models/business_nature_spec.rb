938076c673bfe7dcf8ee53c4ffd89481a2af7809
Von Christian <vc.halip@gmail.com>
2015-01-05 11:03:21 +0800

    updated Gemfile.lock

---
+++ spec/models/business_nature_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe BusinessNature, :type => :model do
+  describe 'validations' do
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :psic_code }
+  end
+
+  describe 'associations' do
+    it { should have_many :business }
+  end
+end
