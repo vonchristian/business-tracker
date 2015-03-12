85b68dd93663756f46ce143e42e9389ea481e970
Von Christian <vc.halip@gmail.com>
2015-03-12 16:54:54 +0800

    remove activities controller

--- spec/policies/business_policy_spec.rb
+++ spec/policies/business_policy_spec.rb
@@ -8,34 +8,34 @@ describe BusinessPolicy do
   context "for an application officer" do
     let(:user) { FactoryGirl.create(:user, role: 'application_officer') }
 
-    it { should permit_authorization(:show)    }
+    it { should permit(:show)    }
 
-    it { should permit_authorization(:create)  }
-    it { should permit_authorization(:new)     }
-    it { should permit_authorization(:update)  }
-    it { should permit_authorization(:edit)    }
-    it { should_not permit_authorization(:destroy) }
+    it { should permit(:create)  }
+    it { should permit(:new)     }
+    it { should permit(:update)  }
+    it { should permit(:edit)    }
+    it { should_not permit(:destroy) }
   end
 
   context "for a system administrator" do
     let(:user) { FactoryGirl.create(:user, role: 'system_administrator') }
 
-    it { should permit_authorization(:show)    }
-    it { should permit_authorization(:create)  }
-    it { should permit_authorization(:new)     }
-    it { should permit_authorization(:update)  }
-    it { should permit_authorization(:edit)    }
-    it { should permit_authorization(:destroy) }
+    it { should permit(:show)    }
+    it { should permit(:create)  }
+    it { should permit(:new)     }
+    it { should permit(:update)  }
+    it { should permit(:edit)    }
+    it { should permit(:destroy) }
   end
 
   context "for a payment officer" do
     let(:user) { FactoryGirl.create(:user, role: 'payment_officer') }
 
-    it { should permit_authorization(:show)    }
-    it { should_not permit_authorization(:create)  }
-    it { should permit_authorization(:new)     }
-    it { should permit_authorization(:update)  }
-    it { should_not permit_authorization(:edit)    }
-    it { should_not permit_authorization(:destroy) }
+    it { should permit(:show)    }
+    it { should_not permit(:create)  }
+    it { should permit(:new)     }
+    it { should permit(:update)  }
+    it { should_not permit(:edit)    }
+    it { should_not permit(:destroy) }
   end
 end
